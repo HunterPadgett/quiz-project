879c97f11c30c0f3a9eda5e07bf28554ba4d0cd1
Hunter Padgett <gizmokabanjo@gmail.com>
2022-06-29 19:45:22 -0400

    added for each and ammended children

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,100 @@
+// creating variables via dot.notation
 var countdownTimer = document.getElementById("countdownTimer");
 var startButton = document.getElementById("startHide");
 var questionConEl = document.getElementById("questionHide");
 var questionEl = document.getElementById("getQuestion");
 var answersEl = document.getElementById("answerButtons");
+var scoreStore = document.getElementById("scoreStore")
+var quesTitle = document.getElementById("quesTitle")
 
+// variables
 var secondsLeft = 60;
-var currentQuestion = ''
+var index = 0;
+var correctAnswer = 0;
+
+// set of questions and answers for the quiz
 var questions = [
   {
     question: "What will the code return? Boolean(20 < 60)",
-    choices: ["1. False", "2. Undefined", "3. True", "4. NaN"],
+    choices: ["1. False", "2. Undefined", "3. True",],
     answer: "3. True"
   },
   {
     question: "Which of the following statements will show a message as well as ask for user input in a popup?",
-    choices: ["1. message()", "2. prompt()", "3. alert()", "4. confirm()"],
+    choices: ["1. message()", "2. prompt()", "3. alert()",],
     answer: "2. prompt()"
-  }, 
+  },
   {
     question: "What is the proper syntax of a for loop?",
-    choices: ["1. for(red + blue = green)", "2. for(i++; i < 0; i = 0)", "3. for(loop)", "4. for(i = 0; i > 0; i++)"],
-    answer: "4. for(i = 0; i > 0; i++)"
+    choices: ["1. for(red + blue = green)", "2. for(i = 0; i > 0; i++)", "3. for(loop)"],
+    answer: "2. for(i = 0; i > 0; i++)"
   },
   {
     question: "Which HTML tags do we store our JavaScript file within?",
-    choices: ["1. <script></script>", "2. <header></header>", "3. <footer></footer>", "4. <img></img>"],
+    choices: ["1. <script></script>", "2. <header></header>", "3. <footer></footer>",],
     answer: "1. <script></script>"
   }
 ];
 
 
-//start button
-// timer countdown ** need to figure out how to start it with start button **
-// countdownTimer.addEventListener('click', startGame)
+// start button click
+// triggers timer countdown, hides start button, displays question & answer container
 startButton.addEventListener('click', startGame)
 function startGame() {
   console.log("HERE WE GOOOOOO")
   startButton.setAttribute("style", "display: none;");
-  questionConEl.setAttribute("style", "display: block")
-  currentQuestion = 0
+  questionConEl.setAttribute("style", "display: flex")
+
   countdownTimer.textContent = secondsLeft;
 
-  var timerSet = setInterval(function() {
-    
+
+  var timerSet = setInterval(function () {
+
     secondsLeft--;
     countdownTimer.textContent = "Time: " + secondsLeft;
 
-    if(secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       clearInterval(timerSet);
-      countdownTimer.textContent = "Times up!" 
+      countdownTimer.textContent = "Times up!"
     }
-  }, 1000); 
+  }, 1000);
 
-  setNextQuestion()
+
+  retrieveQuestions();
+}
+
+function retrieveQuestions() {
+  var questionPopulate = questions[index];
+  quesTitle.textContent = questionPopulate.question;
+  answersEl.innerHTML = '';
+  questionPopulate.choices.forEach(function (choice) {
+    var buttonPopulate = document.createElement('button');
+    buttonPopulate.setAttribute('value', choice);
+    buttonPopulate.textContent = choice; //got check act to set class and value
+    answersEl.append(buttonPopulate); // adds the elements to the parent ChoiceEl
+    buttonPopulate.classList.add("button"); // applies  class='answer' styling
+    console.log(buttonPopulate);
+    buttonPopulate.addEventListener("click", selectAnswer);
+  })
 }
 
 
-function setNextQuestion() {
-  showQuestion(currentQuestion)
-}
-
-function showQuestion(questions) {
-  questionEl.innerText = question.question
-}
-
+// alerts user if right or wrong question was selected and gives time penalty 
 function selectAnswer() {
-
+  var clickedOption = event.target.textContent;
+  var correctOption = questions[index].answer;
+  if (clickedOption === correctOption) {
+    correctAnswer++;
+    scoreStore.textContent = "Score: " + correctAnswer;
+    console.log(correctAnswer)
+    index++;
+    alert("Good Job")
+    retrieveQuestions();
+  } else {
+    secondsLeft -= 5;
+    countdownTimer.textContent = secondsLeft;
+    alert("incorrect! You lose 5 seconds!");
+    // index++
+    retrieveQuestions();
+  }
 }
-
-
-setTimer();
-startGame();
-setNextQuestion();
-showQuestion();
-
-    
